ed579b6631777876b66d12acb415aa7c999542f4
MartinKurus <89841634+MartinKurus@users.noreply.github.com>
2021-09-09 11:12:20 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace PositiveNegative
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm palub kasutaja sisestada numbrit -10 kuni 10
+            //programm kontrollib numbrit
+            //kui number on positiivne, konsool kuvab "positiivne"
+            //kui number on negatiivne, konsool kuvab "null"
+            Console.WriteLine("Sisesta üks number -10 kuni 10");
+            int Number = Convert.ToInt32(Console.ReadLine());
+           
+                if (Number < 0)
+            {
+                    Console.WriteLine("Number on negatiivne");
+            }
+            
+            else if (Number > 0)
+            {
+                Console.WriteLine("Number on positiivne");
+            }
+            
+             else
+            { 
+                Console.WriteLine("Number on null");
+            }
+        }
+    }
+}
